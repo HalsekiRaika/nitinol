5981076058d38e9afc44baf1e9590c98999efc4c
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2025-03-18 14:41:28 +0900

    :truck: - Added new persistence processes for in-memory and SQLite storage

---
+++ examples/3.persistence-process-sqlite/src/main.rs
@@ -0,0 +1,105 @@
+use async_trait::async_trait;
+use nitinol::{Command, EntityId, Event, ToEntityId};
+use nitinol::process::manager::ProcessManager;
+use nitinol::process::persistence::{PersistenceExtension, WithPersistence};
+use nitinol::process::{Applicator, Context, Process, Publisher};
+use nitinol_sqlite_adaptor::store::SqliteEventStore;
+use serde::{Deserialize, Serialize};
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[derive(Debug, Clone, Command)]
+pub enum DomainCommand {
+    ChangeName { new: String },
+    Delete,
+}
+
+#[derive(Debug, Clone, Event, Deserialize, Serialize)]
+#[persist(enc = "serde_json::to_vec", dec = "serde_json::from_slice")]
+pub enum DomainEvent {
+    ChangedName { new: String },
+    Deleted,
+}
+
+#[derive(Debug, Clone)]
+pub struct Aggregate {
+    name: String
+}
+
+#[async_trait]
+impl Process for Aggregate {
+    fn aggregate_id(&self) -> EntityId {
+        "aggregate".to_entity_id()
+    }
+    
+    async fn start(&self, _: &mut Context) {
+        tracing::debug!("Start: {:?}", self);
+    }
+    
+    async fn stop(&self, _: &mut Context) {
+        tracing::debug!("Stop: {:?}", self);
+    }
+}
+
+impl WithPersistence for Aggregate {}
+
+#[async_trait]
+impl Publisher<DomainCommand> for Aggregate {
+    type Event = DomainEvent;
+    type Rejection = anyhow::Error;
+    
+    #[tracing::instrument(skip_all)]
+    async fn publish(&self, command: DomainCommand, _: &mut Context) -> Result<Self::Event, Self::Rejection> {
+        let ev = match command {
+            DomainCommand::ChangeName { new } => DomainEvent::ChangedName { new },
+            DomainCommand::Delete => DomainEvent::Deleted,
+        };
+        tracing::debug!("Accept command. published event: {:?}", ev);
+        Ok(ev)
+    }
+}
+
+#[async_trait]
+impl Applicator<DomainEvent> for Aggregate {
+    #[tracing::instrument(skip_all)]
+    async fn apply(&mut self, event: DomainEvent, ctx: &mut Context) {
+        self.persist(&event, ctx).await;
+        
+        tracing::debug!("Accept event: {:?}", event);
+        match event {
+            DomainEvent::ChangedName { new } => {
+                self.name = new;
+            }
+            DomainEvent::Deleted => {
+                ctx.poison_pill().await;
+            }
+        }
+        tracing::debug!("current state: {:?}", self);
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    tracing_subscriber::registry()
+        .with(EnvFilter::new("trace"))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    
+    let eventstore = SqliteEventStore::setup("sqlite://:memory:").await?;
+    
+    let system = ProcessManager::with_extension(|ext| {
+        ext.install(PersistenceExtension::new(eventstore))
+    })?;
+    
+    let aggregate = Aggregate { name: "name".to_string() };
+    let refs = system.spawn(aggregate, 0).await?;
+    
+    let ev = refs.publish(DomainCommand::ChangeName { new: "new name".to_string() }).await??;
+    refs.apply(ev).await?;
+    
+    let ev = refs.publish(DomainCommand::Delete).await??;
+    refs.apply(ev).await?;
+    Ok(())
+}
