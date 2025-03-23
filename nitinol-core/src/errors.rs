72ebad58d73cae3fcc9cecad6c86c2c84d38b043
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2025-03-23 19:16:00 +0900

    :fire: - Simplified error handling that had been implemented manually with `thiserror`

--- nitinol-core/src/errors.rs
+++ nitinol-core/src/errors.rs
@@ -1,45 +1,21 @@
 use std::error::Error;
-use std::fmt::{Display, Formatter};
 
-
-#[cfg(feature = "markers")]
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
+#[error("Failed to serialize event: {0}")]
 pub struct SerializeError(Box<dyn Error + Sync + Send>);
 
-#[cfg(feature = "markers")]
 impl<E: serde::ser::Error + Sync + Send + 'static> From<E> for SerializeError {
     fn from(value: E) -> Self {
         Self(Box::new(value))
     }
 }
 
-#[cfg(feature = "markers")]
-impl Display for SerializeError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Failed to serialize event: {}", self.0)
-    }
-}
-
-#[cfg(feature = "markers")]
-impl Error for SerializeError {}
-
-#[cfg(feature = "markers")]
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
+#[error("Failed to deserialize event: {0}")]
 pub struct DeserializeError(Box<dyn Error + Sync + Send>);
 
-#[cfg(feature = "markers")]
 impl<E: serde::de::Error + Sync + Send + 'static> From<E> for DeserializeError {
     fn from(value: E) -> Self {
         Self(Box::new(value))
     }
 }
-
-#[cfg(feature = "markers")]
-impl Display for DeserializeError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Failed to deserialize event: {}", self.0)
-    }
-}
-
-#[cfg(feature = "markers")]
-impl Error for DeserializeError {}
