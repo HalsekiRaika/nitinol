57958949bc51c3649808ff66d368513f7dbb35ba
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2025-03-25 13:10:03 +0900

    :art: - Corrected the scope of the `global` module publication.

--- nitinol-projection/src/lib.rs
+++ nitinol-projection/src/lib.rs
@@ -1,12 +1,12 @@
 pub mod errors;
 pub mod projection;
-
 mod fixtures;
 pub mod projector;
 pub mod resolver;
 
-#[cfg(feature = "global")]
 mod global;
 
+pub use self::global::set_global_projector;
+
 #[cfg(feature = "global")]
-pub use self::global::*;
+pub use self::global::get_global_projector;
